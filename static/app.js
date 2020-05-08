dac18cd2d71716ac1523025d3a8b98991de432f6
ngafron <54723624+ngafron@users.noreply.github.com>
2020-05-07 21:50:44 -0500

    Add files via upload

--- static/app.js
+++ static/app.js
@@ -1,44 +1,42 @@
-function buildMetadata(sample) {
-  d3.json("winemag-data-130k-v2.json").then(data => {
-    var metaData = data[0];
-    console.log(metaData.points);
-//     var resultArray = metaData.filter(metaDataObj => metaDataObj.id == sample);
-//     console.log("resultArray: " + resultArray);
-//     var result = resultArray[0];
-//     var panel = d3.select("#sample-metadata");
-//     panel.html("");
-//     Object.entries(result).forEach(([key, value]) => {
-//       panel.append("h6").text(`${key.toUpperCase()}: ${value}`);
-//     });
-  });
-};
+const rating = document.getElementById("rating");
+const country = document.getElementById("country");
+const province = document.getElementById("province");
+const region = document.getElementById("region");
+const variety = document.getElementById("variety");
+const form = document.getElementById("form");
+const errorElement = document.getElementById("error");
 
-//function to initiate plots
-function init() {
-    d3.json("winemag-data-130k-v2.json").then(function(data) {
-      var wines = data;
-      console.log(wines);
-      //grab a reference to the dropdown select
-      var selector = d3.select("#selDataset");
-
-      //use the list of sample names to populate the select options
-      wines.forEach(wine => {
-        selector
-          .append("option")
-          .text(wine.points)
-          .property("value", wine);
-      });
-
-      //use the first sample from list to build the initial plots
-      const firstSample = wines[0].points;
-      buildMetadata(firstSample);
-    });
-}
-
-// function optionChanged(newSample) {
-//     console.log(`sample changed to ${newSample}`);
-//     //fetch new data and build charts
-//     buildMetadata(newSample);
-// }
-
-init();
+d3.json("winemag-data-130k-v2.json").then(function(data) {
+  wines = data
+  console.log(wines);
+  let messages = [];
+  wines.forEach(drinks => {
+    // console.log(drinks.country)
+    function validateRating() {
+      if (rating !== drinks.points) {
+        console.log(rating)
+        message.push('Rating is not valid');
+      }
+    }
+    function validateCountry() {
+      if (country !== drinks.country) {
+        message.push('Country is not valid');
+      }
+    }
+    function validateProvince() {
+      if (province !== drinks.province) {
+        message.push('Province is not valid');
+      }
+    }
+    function validateRegion() {
+      if (region !== drinks.region_1 || region !== drinks.region_2) {
+        message.push('Region is not valid');
+      }
+    }
+    function validateVariety() {
+      if (variety !== drinks.variety) {
+        message.push('Variety is not valid');
+      }
+    }
+    })
+  })
